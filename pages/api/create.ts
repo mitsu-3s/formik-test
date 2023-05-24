797fbd9a20011a7ae48fca5b046dafe107534b92
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-05-24 09:33:06 +0900

    Create API Files

---
+++ pages/api/create.ts
@@ -0,0 +1,14 @@
+// フォームのAPI
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'POST') {
+        try {
+            const { email, password } = req.body
+            console.log(req.body)
+            res.status(200).json({ message: 'OK' })
+        } catch (e) {
+            res.status(500).json({ message: 'Miss' })
+        }
+    }
+}
